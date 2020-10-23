5e8c9d1222f47c09d414f0b77916c9ae2a74e255
Francesco Maestroni <francescomaestroni1@gmail.com>
2020-10-23 16:29:05 +0200

    Correzione mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,13 +1,23 @@
-Mail
+var accessAllowed = [ "francescomaestroni@gmail.com", "giuseppebianchi@gmail.com",
+"marcorossi@gmail.com",
+"pincopallino@gmail.com",
+"tiziocaio@gmail.com"
+];
 
 var mailUtente = prompt("Inserisci la tua mail");
 
-var accessAllowed = ["francescomaestroni@gmail.com", "giuseppebianchi@gmail.com", "marcorossi@gmail.com", "pincopallino@gmail.com", "tiziocaio@gmail.com"];
+var validMail = false;
 
 for (var i = 0; i < accessAllowed.length; i++) {
   if (mailUtente == accessAllowed[i]) {
-    console.log("Accesso consentito")
-  } else {
-    console.log("Accesso negato")
+    validMail = true;
   }
+};
+
+var mailResult = document.getElementById("output");
+
+if (validMail == true) {
+  mailResult.innerHTML = "Accesso consentito"
+} else {
+  mailResult.innerHTML = "Accesso negato"
 };
